3fc67473b7e4eedffbce51a3facf9276a1cef1a5
kachgundevaibhav7561-design <kachgundevaibhav7561@gmail.com>
2025-12-08 13:29:57 +0530

    to learn c

---
+++ my first program.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+	printf("hello c programming\n");
+	printf("my first program");
+	return 0;
+}
---
+++ vk.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+ 	int a=10;
+	int b=23;
+	int c=a+b;
+	printf("c",c);
+	return 0;
+}
